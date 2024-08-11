450fbfca12f2eb6a14809df6785bffd9f31147d8
Kaizar R <dhabukro@gmail.com>
2024-08-10 21:34:27 -0400

    Trained NODE

--- Project/AlgaeODE.jl
+++ Project/AlgaeODE.jl
@@ -1,25 +1,5 @@
-using JLD, Lux, DiffEqFlux, DifferentialEquations, Optimization, OptimizationOptimJL, Random, Plots
-using ComponentArrays, OptimizationOptimisers
-
-# Number of days to simulate
-N_days = 300 #test
-
-# Initial conditions for algae biomass and nutrients
-A0 = 0.01  # Initial algae concentration
-N0 = 1.0   # Initial nutrient concentration
-u0 = [A0, N0]  # [Algae, Nutrient]
-
-# Parameters for the model
-p0 = Float64[
-    0.1,  # r - Intrinsic growth rate
-    1.0,  # K - Carrying capacity
-    0.5,  # k - Half-saturation constant for nutrient uptake
-    0.01, # dN - Nutrient depletion rate
-]
-
 # Time span for simulation
 tspan = (0.0, Float64(N_days))
-datasize = N_days
 t = range(tspan[1], tspan[2], length=datasize)
 
 # Define the ODE function for algal bloom growth
@@ -38,8 +18,50 @@ prob = ODEProblem(AlgalBloom!, u0, tspan, p0)
 # Solve the ODE
 sol = solve(prob, Tsit5(), saveat=t)
 
+# Plotting the results
 plot(t, sol[1, :], label="Algae Concentration A(t)")
 plot!(t, sol[2, :], label="Nutrient Concentration N(t)")
 xlabel!("Time (days)")
 ylabel!("Concentration")
-title!("Algal Bloom Growth Model")
+title!("Compressed Algal Bloom Growth Model")
+
+# Neural Network Architecture
+dudt_nn = Lux.Chain(Lux.Dense(2, 50, sigmoid), Lux.Dense(50, 2))
+p_nn, st_nn = Lux.setup(rng, dudt_nn)
+
+# Define the Neural ODE
+proba_neuralode = NeuralODE(dudt_nn, tspan, Tsit5(), saveat=t)
+
+# Prediction function
+function predict_neuralode(p)
+    Array(proba_neuralode(u0, p, st_nn)[1])
+end
+
+# Loss function
+function loss_neuralode(p)
+    pred = predict_neuralode(p)
+    loss = sum(abs2, sol .- pred)
+    return loss, pred
+end
+
+# Callback to observe training
+callback = function (p, l, pred; doplot=true)
+    display("Current Loss: $(l)")
+    if doplot
+        plt = plot(t, sol[1, :], label="Data", title="Prediction vs. Data", xlabel="Time (days)", ylabel="Concentration")
+        plot!(plt, t, pred[1, :], linestyle=:dash, label="Prediction")
+        display(plot(plt))
+    end
+    return false
+end
+
+# Initialize parameters
+pinit = ComponentArray(p_nn)
+
+# Optimization setup
+adtype = Optimization.AutoZygote()
+optf = Optimization.OptimizationFunction((x, p) -> loss_neuralode(x), adtype)
+optprob = Optimization.OptimizationProblem(optf, pinit)
+
+# Train the model
+result_neuralode = Optimization.solve(optprob, OptimizationOptimisers.Adam(0. 1), callback=callback, maxiters=1000)
